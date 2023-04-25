993bcf3063c64eccc959f9f2650f9abc7cc12206
jaylinrose <jaylin.manuel02@gmail.com>
2023-04-25 14:30:47 -0700

    created async function to retrieve book list and display

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,21 @@
 
 // Your Code Here
+async function getBookList() {
+    const response = await fetch('http://localhost:9001/updateBook')
+    const result = await response.json()
+  result.forEach(bookQuantityChange)
+
+}
+
+
+
+async function bookQuantityChange() {
+    let root = document.querySelector('#root')
+    
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+   
+
+
+}
